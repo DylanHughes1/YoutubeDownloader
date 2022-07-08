5e6333ab56b29ecd2caa66d3e95520a7b1518c2b
Dylan Hughes <dylan_21174@hotmail.com>
2022-07-08 13:39:30 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from tkinter import *
+from tkinter import messagebox as MessageBox
+from pytube import YouTube
+
+
+def download():
+    url = YouTube(str(link.get()))
+    video = url.streams.get_highest_resolution()
+    video.download()
+    Label(window, text="Video Descargado", font="calibri 14").place(x=180, y=200)
+
+
+def initRoot():
+    root = Tk()
+    root.geometry('500x300')
+    root.resizable(0, 0)  # makes the window adjustable with its features
+    root.title('Youtube Downloader')
+    return root
+
+
+def initLabels():
+    Label(window, text="Descarga videos de Youtube gratis", font='calibri 16 bold').pack()
+    link = StringVar()  # Specifying the variable type
+    Label(window, text="Pega tu link aca padre", font='calibri 15 bold').place(x=150, y=55)
+    link_enter = Entry(window, width=70, textvariable=link).place(x=30, y=85)
+    return link
+
+
+def popup():
+    MessageBox.showinfo("Sobre mi", " Si")
+
+
+window = initRoot()
+link = initLabels()
+
+menubar = Menu(window)
+window.config(menu=menubar)
+helpmenu = Menu(menubar, tearoff=0)
+
+menubar.add_cascade(label="Mas informacion", menu=helpmenu)
+helpmenu.add_command(label="Sobre el autor", command=popup)
+menubar.add_command(label="Salir", command=window.destroy)
+
+Button(window, text='Descargar', font='san-serif 16 bold', bg='lightblue', padx=2, command=download).place(x=190, y=150)
+
+window.mainloop()
